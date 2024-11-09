c58fc2f19ddb6fe333a9209a18ee04dc834f4ecb
aalopezb <a.alexander.l.b@gmail.com>
2024-11-08 21:46:14 -0500

    feat: :sparkles: Creating the page in C#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from ASP.NET Core!");
+app.MapGet("/", () => "Hello World with C#!");
 
 app.Run();
